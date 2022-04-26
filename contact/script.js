19d7ba55ec9dfff97ef9c305a0b6779f9b99de21
Ethan <38486990+ByCubed7@users.noreply.github.com>
2022-04-26 09:53:17 +0100

    Moved Contact Script correctly

--- contact/script.js
+++ contact/script.js
@@ -2,9 +2,11 @@
 function SendMessage()
 {
     // Get Message
-
     var messageBox = document.getElementById("message");
     var message = messageBox.value;
+
+    var authorBox = document.getElementById("author");
+    var author = author.value;
 
     // 'Security' to stop basic webcrawlers from just picking this up
     var key = "U2FsdGVkX18/XGacgV1SWuz2CZFWebrIBH5AdxoEkMSbsEAohJWA07tOaA2xzRLS6+d2YuQP8u1aPpSP1m1XfZdY1uSThlwp340JaNzxlhAEn/QUqG4XXjrpS3LtMY2Z";
@@ -13,7 +15,7 @@ function SendMessage()
     // Embed
     var myEmbed = {
         author: { name: "ByCubed7.github.io observation." },
-        title: "A message has been sent!",
+        title: "A message from _____ has been sent!",
         description: message,
         color: parseInt("#ff0000".replace("#", ""), 16)
     };
